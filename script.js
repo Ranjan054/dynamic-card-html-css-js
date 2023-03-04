93bb587989d9b2891e5a726421a5b66f377f83d3
Ranjan <rk0171666@gmail.com>
2023-03-04 15:07:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,376 @@
+let data = [
+    {
+        "title": "check data",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video1",
+        "videoUrl": "https://video.com/video1"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video2",
+        "videoUrl": "https://video.com/video2"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video3",
+        "videoUrl": "https://video.com/video3"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video4",
+        "videoUrl": "https://video.com/video4"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video5",
+        "videoUrl": "https://video.com/video5"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video6",
+        "videoUrl": "https://video.com/video6"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video7",
+        "videoUrl": "https://video.com/video7"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video8",
+        "videoUrl": "https://video.com/video9"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video10",
+        "videoUrl": "https://video.com/video10"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video11",
+        "videoUrl": "https://video.com/video11"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video12",
+        "videoUrl": "https://video.com/video12"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video13",
+        "videoUrl": "https://video.com/video13"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video14",
+        "videoUrl": "https://video.com/video14"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video15",
+        "videoUrl": "https://video.com/video15"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video16",
+        "videoUrl": "https://video.com/video16"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video17",
+        "videoUrl": "https://video.com/video17"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video18",
+        "videoUrl": "https://video.com/video18"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video19",
+        "videoUrl": "https://video.com/video19"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video20",
+        "videoUrl": "https://video.com/video20"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo1.png"
+    },
+    {
+        "title": "Direct Investment | ",
+        "subTitle": "How We Invest",
+        "image": "./assets/images/card-img1.png",
+        "logo": "./assets/images/logo1.png",
+        "videoTitle": "video21",
+        "videoUrl": "https://video.com/video21"
+    },
+    {
+        "title": "af Private Equity and al to acuire Enviro, the leading provider of mission critical Biopharma Transport Service",
+        "subTitle": "",
+        "image": "./assets/images/card-img2.png",
+        "logo": "./assets/images/logo2.png"
+    }
+];
+
+const cardWrapper = document.getElementById("card-wrapper");
+const btn = document.getElementById("btn");
+const modal = document.getElementById("modal");
+const close = document.getElementById("close");
+
+let cardInnerNode;
+let filterCardData;
+let initialCard = 4;
+
+const showCards = () => {
+    filterCardData.forEach((el, index) => {
+        let card = document.createElement("div");
+        card.className = "card"
+        let even = index % 2 === 0;
+        if (even) {
+            cardInnerNode = `
+                <img class="card-img" src=${el.image} alt="discussion">
+                <div class="bottom-text">
+                    <p class="heading">
+                        ${el.title}
+                        <span class="subHeading">${el.subTitle}</span>
+                    </p>
+                    <img onClick=showModal("${el.videoTitle}") class="play-icon" src="./assets/images/play.png" alt="icon">
+                </div>
+            `;
+        } else {
+            cardInnerNode = `
+                <div class="top-text">
+                <p class="para">
+                ${el.title}
+                </p>
+                <img class="company-logo" src=${el.logo} alt="icon">
+                </div>
+                <img class="card-img" src=${el.image} alt="Investment discussion">
+           `;
+
+        }
+        card.innerHTML = cardInnerNode;
+        cardWrapper.appendChild(card)
+    });
+};
+
+//this function will print data from end of last array length to new 4 cards
+
+const sliceCards = (limit) => {  
+    filterCardData = data.slice(limit-4, limit);
+    showCards();
+};
+
+
+//this function will print data from 0 to till limit
+
+// const sliceCards = (limit) => {
+//     cardWrapper.innerHTML = "";
+//     filterCardData = data.slice(0, limit);
+//     showCards();
+// };
+
+sliceCards(initialCard);
+
+const showModal = (title) => {
+    modal.innerHTML = ""
+    let div = document.createElement("div");
+    div.className = "player-wrapper";
+    let modalNode = `
+    <img onClick=closeModal() id="close" src="./assets/images/close.svg" alt="close">
+    <h3 class="modal-title">
+       ${title}
+    </h3>
+    <div class="player">
+        video will play here
+    </div>
+    `;
+    div.innerHTML = modalNode;
+    modal.appendChild(div);
+    modal.style.display = "flex";
+};
+
+const closeModal = () => {
+    modal.style.display = "none";
+};
+
+btn.addEventListener("click", () => {
+    if (data.length > initialCard) {
+        initialCard += 4;
+        sliceCards(initialCard)
+    } else (
+        alert("This is end of card list")
+    )
+});
+
+
+
